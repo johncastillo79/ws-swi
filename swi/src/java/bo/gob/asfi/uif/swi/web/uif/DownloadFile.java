b644a456affd0fd3ec0ce4ac6c0ba4d4560f7450
john.gnu@gmail.com <john.gnu@gmail.com@352168e6-5543-ddc1-9830-f2910d53b645>
2013-12-20 14:38:53 +0000

    reparaciones

---
+++ swi/src/java/bo/gob/asfi/uif/swi/web/uif/DownloadFile.java
@@ -0,0 +1,120 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bo.gob.asfi.uif.swi.web.uif;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author marcelomos
+ */
+@WebServlet(name = "DownloadFile", urlPatterns = {"/DownloadFile"})
+public class DownloadFile extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet DownloadFile</title>");
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>Servlet DownloadFile at " + request.getContextPath() + "</h1>");
+//            out.println("</body>");
+//            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+
+        String name = request.getParameter("name");
+        String requestid = request.getParameter("requestid");
+        String filename = name + ".xml";
+
+        String xmlStr = requestid;//request.toString();//"<xml><data>test</data><requestId>"+requestid+"<requestId><name>"+name+"<name></xml>";
+
+        downloadStream(xmlStr, filename, response);
+
+        processRequest(request, response);
+
+
+
+    }
+
+    private void downloadStream(String xmlStr, String filename, HttpServletResponse resp) {
+        try {
+
+            resp.setContentType("application/x-download");
+            resp.setHeader("Content-Disposition", "attachment;filename=\"" + filename + "\"");
+
+            PrintWriter out = resp.getWriter();
+            out.println(xmlStr);
+
+
+        } catch (IOException e) {
+//			e.printStackTrace();
+        }
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
